66404fbaf4bf93ea083e714cfe4f511603916a1b
Benedikt Lotter <blotter123@gmail.com>
2013-03-10 11:22:27 +0100

    location & map implemented, several bugs to fix

---
+++ Notes/BTLLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  BTLLocationManager.h
+//  Notes
+//
+//  Created by Benedikt Lotter on 3/9/13.
+//  Copyright (c) 2013 Benedikt Lotter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTLLocationManager : NSObject
+
+@end
---
+++ Notes/BTLLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  BTLLocationManager.m
+//  Notes
+//
+//  Created by Benedikt Lotter on 3/9/13.
+//  Copyright (c) 2013 Benedikt Lotter. All rights reserved.
+//
+
+#import "BTLLocationManager.h"
+
+@implementation BTLLocationManager
+
+@end
