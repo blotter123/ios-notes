6961a227ef9354dafc10ae731f934603d52fb87d
Benedikt Lotter <blotter123@gmail.com>
2013-03-11 23:48:31 -0400

    4square location selection completed

---
+++ Notes/BTLLocationSelectController.h
@@ -0,0 +1,13 @@
+//
+//  BTLLocationSelectController.h
+//  Notes
+//
+//  Created by Benedikt Lotter on 3/11/13.
+//  Copyright (c) 2013 Benedikt Lotter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTLLocationSelectController : UITableViewController
+
+@end
